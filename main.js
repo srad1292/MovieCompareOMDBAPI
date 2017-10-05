d1e8bf4221cf89aeb9bdb510fb547e7ca8ea0dcb
srad1292 <smr1292@yahoo.com>
2017-10-05 17:53:20 -0400

    Query fills out a table with results up to 5 results

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 $(document).ready(function() {
 	var $Form = $('form');
+	var qResults;
 	var $Container = $('#resultsContainer');
 	$Container.hide();
 	
 	$Form.on('submit', function(p_oEvent){
-		fillOutResults(p_oEvent);       
+		getResults(p_oEvent);     
 	});
 
-
-	function fillOutResults(p_oEvent) {
-		var sUrl, sMovie, oData;
+	function getResults(p_oEvent){
+		var sUrl, sMovie;
+		var oData;
 	    p_oEvent.preventDefault();
 		sMovie = $Form.find('#title').val();
 	    //Get movie by id
@@ -17,49 +18,64 @@ $(document).ready(function() {
 		//Search for movies with given text
 		sUrl = 'https://api.themoviedb.org/3/search/movie?api_key=97058a18f3b899a2e57452cec18ee321&query=' + sMovie;
 	    $.ajax(sUrl, {
+	    	
 	        complete: function(p_oXHR, p_sStatus){
 	            oData = $.parseJSON(p_oXHR.responseText);
 	            console.log("Results: " + oData.results.length);	
 				if (oData.Response === "False") {
 					$Container.hide();
-				} 
-				else {
-					var row = 0;
-					var col = 0;
-					var currentResult = oData.results[row];
-					$("#resultsTable").find("td").each(function() {
-						if(row < oData.results.length){
-							if(col === 0){
-								$(this).html((row+1) + "");
-								col+=1;
-							}
-							
-							else if(col === 1) {
-								$(this).html(currentResult.title);
-								col++;
-							}
-							else if(col === 2) {
-								$(this).html(currentResult.release_date);
-								col++
-							}
-							else if(col === 3) {
-								$(this).html(currentResult.overview);
-								col = 0;
-								row += 1;
-								if(oData.results.length > row){
-									currentResult = oData.results[row];
-								}
-							}
-						}
-
-					});
-
-					//$("#resultsOne:nth-child(2)").text(oData.title);
-					
-					$Container.show();
-					
 				}
+				else{
+					setData(oData);
+					fillOutResults();  
+				} 
 	        }
-	    }); 
+	    });
+
 	}
+
+	function fillOutResults() {
+		var row = 0;
+		var col = 0;
+		var currentResult = qResults.results[row];
+		$("#resultsTable").find("td").each(function() {
+			if(row < qResults.results.length){
+				if(col === 0){
+					$(this).html((row+1) + "");
+					col+=1;
+				}			
+				else if(col === 1) {
+					$(this).html(currentResult.title);
+					col++;
+				}
+				else if(col === 2) {
+					$(this).html(currentResult.release_date);
+					col++
+				}
+				else if(col === 3) {
+					$(this).html(currentResult.overview);
+					col = 0;
+					row += 1;
+					if(qResults.results.length > row){
+						currentResult = qResults.results[row];
+					}
+				}
+			}
+		}); 
+		$Container.show();
+	}
+
+	function setData(oData) {
+		qResults = oData;
+	}
+
+
+	
 });
+
+
+
+
+
+
+
