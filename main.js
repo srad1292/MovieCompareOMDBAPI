6404b1092db3aa9dd6b1b3ca93f3d1ff6e500f66
srad1292 <smr1292@yahoo.com>
2017-10-19 11:06:26 -0400

    Display table is sortable by name, date, description, and rating

--- main.js
+++ main.js
@@ -18,6 +18,7 @@ Steps done
 14 - Removing an item from 'your movies' works
 15 - Numbers update accurately
 16 - Clicking to add doesn't add something already added
+17 - Sort the 'your movies' table
 
 Should - 
 1 - A 0 rating be left as 0 or changed to N/A (how does each affect sorting)
@@ -25,11 +26,9 @@ Should -
 
 
 Steps left
-
-1 - Sort the 'your movies' table
-2 - Make tables look nice(maybe use bootstrap)
-3 - Clean up UI
-4 - Clean up code
+1 - Make tables look nice(maybe use bootstrap)
+2 - Clean up UI
+3 - Clean up code
 */
 
 $(document).ready(function() {
@@ -251,9 +250,66 @@ $(document).ready(function() {
 		$Container.hide();
 	});
 
-	
 
-
+	$("#titleHeading").click(function(){sortTable(1)});
+	$("#yearHeading").click(function(){sortTable(2)});
+	$("#descriptionHeading").click(function(){sortTable(3)});
+	$("#ratingHeading").click(function(){sortTable(4)});
+	function sortTable(n) {
+  		var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  		table = document.getElementById("displayTable");
+  		switching = true;
+  		// Set the sorting direction to ascending:
+  		dir = "asc";
+  		/* Make a loop that will continue until
+  		no switching has been done: */
+  		while (switching) {
+  		  // Start by saying: no switching is done:
+  		  switching = false;
+  		  rows = table.getElementsByTagName("tr");
+  		  /* Loop through all table rows (except the
+  		  first, which contains table headers): */
+  		  for (i = 1; i < (rows.length - 1); i++) {
+  		    // Start by saying there should be no switching:
+  		    shouldSwitch = false;
+  		    /* Get the two elements you want to compare,
+  		    one from current row and one from the next: */
+  		    x = rows[i].getElementsByTagName("td")[n];
+  		    y = rows[i + 1].getElementsByTagName("td")[n];
+  		    /* Check if the two rows should switch place,
+  		    based on the direction, asc or desc: */
+  		    if (dir == "asc") {
+  		      if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+  		        // If so, mark as a switch and break the loop:
+  		        shouldSwitch= true;
+  		        break;
+  		      }
+  		    } else if (dir == "desc") {
+  		      if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+  		        // If so, mark as a switch and break the loop:
+  		        shouldSwitch= true;
+  		        break;
+  		      }
+  		    }
+  		  }
+  		  if (shouldSwitch) {
+  		    /* If a switch has been marked, make the switch
+  		    and mark that a switch has been done: */
+  		    rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+  		    switching = true;
+  		    // Each time a switch is done, increase this count by 1:
+  		    switchcount ++;
+  		  } else {
+  		    /* If no switching has been done AND the direction is "asc",
+  		    set the direction to "desc" and run the while loop again. */
+  		    if (switchcount == 0 && dir == "asc") {
+  		      dir = "desc";
+  		      switching = true;
+  		    }
+  		  }
+  		}
+  		setNumbers();
+	}
 });
 
 
